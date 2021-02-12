9e64e94c13955b2ba60bd65edea4c0da0a411de2
Justin <justinpessel@gmail.com>
2021-02-12 09:05:29 +0100

    array

--- script_01.js
+++ script_01.js
@@ -2,8 +2,20 @@
 /*** 1.0 Arrays */
 
 
-
-
+//"Ich bin Drazen."
+ausgabe (getSentence("Drazen", "Savi")); //Test
+function getSentence(firstName,familyName) {
+    
+    let gap = " ";
+    let punct = ".";
+    
+    
+    
+    let outputStr = "Hallo" + gap + firstName + gap + familyName + "!";   
+    
+    
+    
+    return outputStr;
 
 
 // Modul: Ausgabe in Konsole | Test:
